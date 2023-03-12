63eeaba121c140a542a896437061284934687d11
Kidusfikadu <124499132+Kidusfikadu@users.noreply.github.com>
2023-03-13 00:49:58 +0300

    Update symbol check.cpp

--- Kidus fikadu/symbol check.cpp
+++ Kidus fikadu/symbol check.cpp
@@ -7,17 +7,20 @@ int main()
     is a digit, uppercase, lowercase or special character*/
     
    char symbol;
-    cout<<"enter any symbol:\n";
-    cin>>symbol;
+     cout<<"enter any symbol:\n";
+     cin>>symbol;
     if (symbol>='A' && symbol<='Z'){
-    cout<<"the symbol "<<symbol<<" is a capital letter(uppercase)";
-    }
-     else if (symbol>='a' && symbol<='z'){
-    cout<<"the symbol "<<symbol<<" is a small letter(lowercase)";}
-     else if(symbol>='0'&& symbol<='9'){
-    cout<<"the symbol "<<symbol<<" is a digit";}
+     cout<<"the symbol "<<symbol<<" is a capital letter(uppercase)";
+     }
+    else if (symbol>='a' && symbol<='z'){
+     cout<<"the symbol "<<symbol<<" is a small letter(lowercase)";
+     }
+    else if(symbol>='0'&& symbol<='9'){
+     cout<<"the symbol "<<symbol<<" is a digit";
+     }
     else {
-    cout<<"the symbol "<<symbol<< " is a special character";
-    }
+     cout<<"the symbol "<<symbol<< " is a special character";
+     }
+
     return 0;
 }
