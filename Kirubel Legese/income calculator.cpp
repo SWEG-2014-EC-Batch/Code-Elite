9e5e52b6ab39b2605e6ccbe844233f0e42b18dd0
Proffesorgreen <124571007+Proffesorgreen@users.noreply.github.com>
2023-03-15 22:07:22 +0300

    Add files via upload

--- Kirubel Legese/income calculator.cpp
+++ Kirubel Legese/income calculator.cpp
@@ -1,27 +1,44 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    float gross salary,worked hours,income tax rate,overtime bonus rate/hour,overtime payment,net salary;
-    cout<<"Enter gross salary of employee ";
-    cin>>gross salary;
-    cout<<"Enter income tax rate of employee";
-    cin>>income tax rate;
-    cout<<"Enter worked hour";
-    cin>>worked hour;
-    if(worked hour>40){
-    cout<<"Enter overtime bonus rate/hour";
-    overtime payment=overtime bonus rate/hour*(worked hour-40);
-    net salary=gross salary-0.07(gross salary)-income tax rate(gross salary)+overtime payment;
-    cout<<"The employee's net salary is "<<net salary<<endl;
-    }
-    else if(worked hour<40){
-    net salary=gross salary-0.07(gross salary)-income tax rate(gross salary);
-    cout<<"The employee's net salary is "<<net salary<<endl;
+int main(){
+    float netSalary,grossSalary,taxRate,bonusRate,workedHours,bonusPayment, pension;
+    int num;
+    cout<<"Enter the number of Employees you want to calculate the net salary for: ";
+    cin>>num;
+    for(int x = 1;x<=num;x++){
+        cout<<"Enter the gross salary of employee number "<<x<<": ";
+        cin>>grossSalary;
+        cout<<"Enter the worked hours of employee number "<<x<<": ";
+        cin>>workedHours;
+        
+        if(workedHours>40){
+            cout<<"Enter the employee's Bonus payment rate per hour(in %): ";
+            cin>>bonusRate;
+            bonusPayment = bonusRate * (workedHours - 40);
+        }
+        else{
+            bonusPayment = 0;
+        }
+        if(grossSalary<200 && grossSalary>=0){
+            taxRate = 0;
+        }
+        else if(grossSalary<600){
+            taxRate = 10;
+        }
+        else if(grossSalary<1200){
+            taxRate = 15;
+        }
+        else if(grossSalary<2000){
+            taxRate = 20;
+        }
+        else if(grossSalary<3500){
+            taxRate = 25;
+        }
+        else if(grossSalary>3500){
+            taxRate = 30;
+        }
+        pension = grossSalary * 0.07;
+        netSalary = (grossSalary - pension + bonusPayment)*(1 - taxRate/100);
+        cout<<"The Net Salary of employee number "<<x<<" is "<<netSalary<<endl;
     }
 }
-    
-    
-    
-    
-    
