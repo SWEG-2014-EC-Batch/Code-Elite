271fd58b5a446d5930d7814621c4de990d3ba5cd
Kirub el <124454603+Kirubel-wondwoson@users.noreply.github.com>
2023-03-16 21:46:18 +0300

    Add files via upload

---
+++ Kirubel-wondwoson/Ex5a_printingPattern.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int k=10;k<=40;k+=10){
+      for(int i=10;i<20;i++){
+        cout<<i<<" ";
+      }
+    cout<<endl;
+    }
+}
---
+++ Kirubel-wondwoson/Ex5b_printingPattern.cpp
@@ -0,0 +1,61 @@
+//Exerscise 5c
+#include<iostream>
+using namespace std;
+int main()
+{/*
+    for(int r=1;r<=4;r++){
+        for(int c=1;c<=7;c++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+*/
+
+ /*for(int r=1;r<=7;r++)
+    {
+        for(int c=1;c<=8;c++)
+        {
+            if(c==1||c==8||r==1||r==7)
+            {
+                cout<<"*"<<" ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+     cout<<endl;  
+    }
+*/
+/*Inverted Half pyramid
+for(int r=1;r<=6;r++)
+    {
+        for(int c=6;c>=r;c--)
+        {
+            cout<<"*  ";
+        }
+        cout<<endl;
+    }
+}
+*/
+/*Half pyramid
+ for(int c=1;c<=6;c++)
+    {
+        for(int r=1;r<=c;r++)
+        {
+            cout<<"*"<<" ";
+        }
+     cout<<endl;
+    }
+}*/
+/*
+  for(int k=1;k<=4;k++){
+        for(char i='A';i<='E';i++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+*/
+
+
+
+}
