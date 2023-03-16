649e0f714785fc528268d32415bf5c8c1ed45f5b
KidusHawoltu <124450313+KidusHawoltu@users.noreply.github.com>
2023-03-16 18:02:45 +0300

    Add files via upload

---
+++ Kidus Hawoltu/Printing_patterns_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<4;++i){
+        for(int j=0;j<7;++j){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
