d60c1bf77f84530c4f08a363cd429bb67cd62a34
KidusHawoltu <124450313+KidusHawoltu@users.noreply.github.com>
2023-03-16 17:59:21 +0300

    It now asks the dimention of the pattern

--- Kidus Hawoltu/Printing_patterns_1.cpp
+++ Kidus Hawoltu/Printing_patterns_1.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 int main(){
-    for(int i=0;i<40;++i){
-        cout<<10+i<<' ';
-        if((i+1)%10 == 0){
+    int row, col, num;
+    cout<<"Enter the number of rows: ";
+    cin>>row;
+    cout<<"Enter the number of columns: ";
+    cin>>col;
+    cout<<"Enter the starting number: ";
+    cin>>num;
+    for(int i=0;i<row*col;++i){
+        cout<<num+i<<' ';
+        if((i+1)%col == 0){
             cout<<endl;
         }
     }
