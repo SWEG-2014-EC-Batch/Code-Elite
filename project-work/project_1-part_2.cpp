f32d263d6b5470902cab326773e8008068f668c0
Kidus Berhane <124568569+Kidusbk@users.noreply.github.com>
2023-04-16 21:50:26 +0300

    Update project_1-part_2.cpp

--- project-work/project_1-part_2.cpp
+++ project-work/project_1-part_2.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    char letter;
 
+    cout<<"Enter a capital letter (A-Z): ";
+    cin>>letter;
+
+    if(letter<'A'){
+        cout<<"You entered invalid letter.";
+        return 0;
+    }
+    
+    n = int(letter) - 64;
+    return 0;
+}
