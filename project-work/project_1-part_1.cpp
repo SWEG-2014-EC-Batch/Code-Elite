361e6be35901da60a4bb81832ba01cb7923fe697
Kidusfikadu <124499132+Kidusfikadu@users.noreply.github.com>
2023-04-17 07:39:08 +0300

    Update project_1-part_1.cpp

--- project-work/project_1-part_1.cpp
+++ project-work/project_1-part_1.cpp
@@ -21,6 +21,55 @@ int main(){
                 break;
 
             case 'C':
+                cout<<endl<<"Enter the number of years: ";
+                cin>>numYears;
+                cout<<"Enter the minimum interest rate(%): ";
+                cin>>minRate;
+                cout<<"Enter the maximum interest rate(%): ";
+                cin>>maxRate;
+                if(minRate<0 || maxRate<0 || numYears<0){
+                    cout<<"Wrong value (negative numbers)."<<endl;
+                    break;
+                }
+                if(minRate>maxRate){
+                    cout<<"Wrong value (minimum rate is greater than maximum rate)"<<endl;
+                    break;
+                }
+                annualDeposit2 = new float[numYears+1];
+                for(int i=0; i<=numYears; ++i){
+                    count2=0;
+                    do{
+                        if(i==0){
+                        cout<<"Enter the amount you deposited at first: ";
+                        }
+                        else{
+                        cout<<"Enter the deposit made after "<<i<<" years: ";
+                        }
+                        cin>>annualDeposit2[i];
+                        if(annualDeposit2[i]<0){
+                            cout<<"Wrong Value. ";
+                            if(count2!=2){
+                                cout<<"Try again.";
+                            }
+                            cout<<endl;
+                            count2+=1;
+                        }
+                    }while(annualDeposit2[i]<0 && count2<3);
+                    if(count2==3){
+                        break;
+                    }
+                }
+                if(count2==3){
+                    break;
+                }
+                rate=(minRate+maxRate)/200;
+                for(int i=0; i<=numYears; ++i){
+                    accountBalance += annualDeposit2[i] * pow((1+rate) , (numYears-i));
+                }
+                cout<<"Your balance after "<<numYears<<" years is "<<accountBalance<<endl;
+                delete [] annualDeposit2;
+
+
                 break;
 
             case 'E':
