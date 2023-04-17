57f80c4b2dbbe3ed9fb5a05b45393ee2ab258e4a
Kirubel Legese <124571007+Proffesorgreen@users.noreply.github.com>
2023-04-17 09:07:23 +0300

    Update project_2.cpp

--- project-work/project_2.cpp
+++ project-work/project_2.cpp
@@ -311,6 +311,128 @@ int main(){
                 break;
 
             case 'B':
+                cout<<endl<<"Do you want the top n students or the top m\% students\nEnter '%' to get top students by percent and any other character for top n students: ";
+                cin>>menu2;
+                if(menu2=='%'){
+                    cout<<"Enter the percentage(0-100): ";
+                    cin>>percent;
+                    if(percent<0 || percent>100){
+                        cout<<"\nThe percentage is out of range\n"<<endl;
+                        break;
+                    }
+                    n=percent*numStud/100;
+                }
+                else{
+                    cout<<"Enter the value of n: ";
+                    cin>>n;
+                    if(n<0 || n>numStud){
+                        cout<<"\nn is out of range (0-"<<numStud<<")\n"<<endl;
+                        break;
+                    }
+                }
+                
+                orders = new int[n];
+                minValue = cgpa[0];
+                for(int i=0; i<numStud; ++i){
+                    if(count2[i] == 3){
+                        continue;
+                    }
+                    if(minValue >= cgpa[i]){
+                        minValue = cgpa[i];
+                        orders[n-1] = i;
+                    }
+                }
+                for(int i=0; i<n; ++i){
+                    if(count2[i] == 3){
+                        continue;
+                    }
+                    order = minValue;
+                    for(int j=0; j<numStud; ++j){
+                        int count=0;
+                        for(int k=0; k<i; ++k){
+                            if(j == orders[k]){
+                                count += 1;
+                                break;
+                            }
+                        }
+                        if(count==1){
+                            continue;
+                        }
+                        if(order <= cgpa[j]){
+                            order = cgpa[j];
+                            orders[i] = j;
+                        }
+                    }
+                    
+                }
+
+                cout<<" ";
+                for(int i=0; i<numCourse; ++i){
+                    cout<<"        ";
+                }
+                if(menu2=='%'){
+                    cout<<"Top "<<percent<<"\% based on CGPA result"<<endl;
+                }
+                else{
+                    cout<<"Top "<<n<<" based on CGPA result"<<endl;
+                }
+
+                cout<<"______________";
+                for(int i=0; i<numCourse; ++i){
+                    cout<<"________________";
+                }
+                cout<<"______________________"<<endl;
+
+                cout<<" Stud. ID       ";
+                for(int i=0; i<numCourse; ++i){
+                    cout<<"    "<<subjectName[i]<<"        ";
+                }
+                cout<<"Average    CGPA"<<endl;
+
+                cout<<"              ";
+                for(int i=0; i<numCourse; ++i){
+                    cout<<"________________";
+                }
+                cout<<endl;
+
+                cout<<"                ";
+                for(int i=0; i<numCourse; ++i){
+                    cout<<"Mark : Grade    ";
+                }
+                cout<<endl;
+
+                cout<<"______________";
+                for(int i=0; i<numCourse; ++i){
+                    cout<<"________________";
+                }
+                cout<<"______________________"<<endl;
+
+                for(int i=0; i<n; ++i){
+                    cout<<' '<<studId[orders[i]]<<"   ";
+                    for(int j=0; j<numCourse; ++j){
+                        if(gradeMark[orders[i]][j] == "NG"){
+                            cout<<"    "<<gradeMark[orders[i]][j]<<" : "<<gradeMark[orders[i]][j]<<"     ";
+                            continue;
+                        }
+                        cout<<"    "<<subjectMark[orders[i]][j]<<" : "<<gradeMark[orders[i]][j]<<"     ";
+                    }
+                    if(temp[orders[i]] == 0){
+                        cout<<"    --        --"<<endl<<endl;
+                        continue;
+                    }
+                    cout<<"    "<<avg[orders[i]]<<"        "<<cgpa[orders[i]]<<endl;
+                    if(i<n-1){
+                        cout<<endl;
+                    }
+                }
+
+                cout<<"______________";
+                for(int i=0; i<numCourse; ++i){
+                    cout<<"________________";
+                }
+                cout<<"______________________"<<endl<<endl;
+                delete []orders;
+               
                 break;
 
             case 'C':
