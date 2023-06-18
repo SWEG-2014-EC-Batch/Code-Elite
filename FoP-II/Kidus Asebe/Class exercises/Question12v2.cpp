118f8ba378b14dd0673969cf621a54336db3b1a2
Kidus Asebe <100912644+Kad-19@users.noreply.github.com>
2023-06-18 15:02:31 +0300

    Add files via upload

---
+++ FoP-II/Kidus Asebe/Class exercises/Question12v2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void calcQuotient(double n1, double n2, double *answer);
+
+int main(){
+    double num1 = 12, num2 = 3, quotient;
+
+    calcQuotient(num1, num2, &quotient);
+    cout<<num1;
+    cout<<"The quotient is: "<<quotient;
+    return 0;
+}
+
+void calcQuotient(double n1, double n2, double *answer){
+   
+    *answer = n1/n2;
+    n1 = 0;
+}
