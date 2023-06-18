3678c4c16422c5fd6689a20e7b6f96dcdfa352c1
Kidusfikadu <124499132+Kidusfikadu@users.noreply.github.com>
2023-06-18 19:04:47 +0300

    Add files via upload

---
+++ FoP-II/Kidus fikadu/Homeworks/HWORK#18.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void calcDiscount(double& price, double disc, bool pof) {
+    if (pof == 0) {
+        disc = price * disc / 100;
+    }
+    price -= disc;
+}
+
+int main() {
+    double price, discount;
+    bool percentageOrFixed;
+    cout << "Enter the price of the item: ";
+    cin >> price;
+    cout << "Enter 0 to calculate price per discount percentage \nor enter 1 to calculate price per fixed discount: ";
+    cin >> percentageOrFixed;
+
+    if (percentageOrFixed) {
+
+        cout << "Enter discount amount: ";
+        cin >> discount;
+    }
+    else {
+        cout << "Enter discount percentage: ";
+        cin >> discount;
+    }
+
+    if (discount < 0) {
+        return 0;
+    }
+
+    calcDiscount(price, discount, percentageOrFixed);
+    if (price <= 0) {
+        return 0;
+    }
+    cout << "The price is: " << price;
+
+}
