dc2871782a7ba87101f440f4ede5b0e05b32b123
Kidus Asebe <100912644+Kad-19@users.noreply.github.com>
2023-06-18 14:09:52 +0300

    Add files via upload

---
+++ FoP-II/Kidus Asebe/Homework Activity/Homework Activity2/Question16.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <ctype.h>
+using namespace std;
+
+void readTime(int&, int&);
+void convert(int&, char&);
+void display(int, int, char);
+
+int main(){
+    char repeat;
+    do{
+        int hours, minutes;
+        char meridian = 'A';
+        readTime(hours, minutes);
+        convert(hours, meridian);
+        display(hours, minutes, meridian);
+
+        cout<<"Do you want the program to repeat? reply 'Y' for yes and 'N' for no: ";
+        cin>>repeat;
+        repeat = toupper(repeat);
+    }while(repeat == 'Y');
+    
+}
+void readTime(int &hr, int &min){
+    cout<<"Enter the time in 24 - hour notaion \nHours: ";
+    cin>>hr;
+    cout<<"Minutes: ";
+    cin>>min;
+}
+void convert(int &hr, char &mer){
+    if(hr >= 13){
+        hr -= 12;
+        mer = 'P';
+    }
+}
+void display(int hr, int min, char mer){
+    cout<<"The time in 12 - hour notation: "<<hr<<":"<<min<<" "<<mer<<"M"<<endl;
+}
