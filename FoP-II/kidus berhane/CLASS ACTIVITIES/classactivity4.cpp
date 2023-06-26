966ce355ec644af2f7538ec117d3806749b98f2e
Kidus Berhane <124568569+Kidusbk@users.noreply.github.com>
2023-06-26 23:30:41 +0300

    Update classactivity4.cpp

--- FoP-II/kidus berhane/CLASS ACTIVITIES/classactivity4.cpp
+++ FoP-II/kidus berhane/CLASS ACTIVITIES/classactivity4.cpp
@@ -1,10 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int fibo(int n)
+int fibonacci(int n);
 
-int main()
-{
-    int num=5
-    cout<<"fibonacci number"<<num<<fib
+int main(){
+    int num = 4;
+    cout<<"Fibonacci number "<<num<<" is: "<<fibonacci(num);
+    return 0;
 }
-int fibo(int n)
+
+int fibonacci(int n){
+    if(n==0 || n==1){
+        return n;
+    }
+    else{
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+}
