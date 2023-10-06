6be6bbe153ae22e72155405ca106f8397255aac5
Kidus Asebe <100912644+Kad-19@users.noreply.github.com>
2023-10-06 21:49:26 +0300

    Update Student_Record_Management_System.cpp

--- FoP-II/Project/Student_Record_Management_System.cpp
+++ FoP-II/Project/Student_Record_Management_System.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <vector>
+#include "main.h"
+using namespace std;
 
+int main() {
+
+    readFromFile();
+
+    char signIn;
+
+    do{
+        cout<<"\nSign in as\nA: Student\nB: Teacher\nC: Staff\n\nEnter E to exit the program\nYour choice: ";
+        cin>> signIn;
+        signIn = toupper(signIn);
+        
+        switch (signIn){
+            case 'A':
+                student();
+                break;
+
+            case 'B':
+                teacher();
+                break;
+                
+            case 'C':
+                staff();
+                break;
+        }
+    }while(signIn == 'A' || signIn == 'B' || signIn == 'C');
+    
+    save();
+
+    return 0;
+}
